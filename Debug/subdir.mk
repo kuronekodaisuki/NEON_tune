cd14ac377bb71c9442d299d074a3aa7470923445
Hiro Matsuoka <kuronekodaisuki@me.com>
2015-06-28 17:00:56 +0900

    prepare for gcc inline assember

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -6,14 +6,28 @@
 CPP_SRCS += \
 ../main.cpp 
 
+C_SRCS += \
+../SDL_rotate.c 
+
 OBJS += \
+./SDL_rotate.o \
 ./main.o 
+
+C_DEPS += \
+./SDL_rotate.d 
 
 CPP_DEPS += \
 ./main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
